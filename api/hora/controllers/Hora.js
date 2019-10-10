eab24f2b90a90a0694580511c9054438933bef3d
Nexon <vinicius@weef.com.br>
2019-10-09 21:10:04 -0300

    subindo pra teste

---
+++ api/hora/controllers/Hora.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/guides/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    create:  (ctx) => {
+        var _ = require('moment');
+        var data = _().unix();
+        ctx.response.body = {
+            data: data
+        };
+        return ctx.response.body;
+    },
+
+    find:  (ctx) => {
+        var _ = require('moment');
+        var data = _().unix();
+        ctx.response.body = {
+            data: data
+        };
+        return ctx.response.body;
+    }
+};
